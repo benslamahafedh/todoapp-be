e6db83a91bbc4998487eee00bd042f9c2a766064
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2021-12-03 15:33:27 +0100

    final database-connection

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const db=require('database_connection');
-db.insert (`INSERT INTO tasks (1,'yh','h',NOW(),'t','h')`);
-db.getData('SELECT * FROM tasks');
+const db=require('./database_connection');
+const Joi =require('joi');
+const express = require("express");
+const app = express();
+app.use(express.json())
+// db.query (`INSERT INTO tasks VALUES(1,'yh','h',NOW(),'t','h');`,'insertion');
+
+
+// db.query (`TRUNCATE TABLE tasks;`,'data cleared')
+
