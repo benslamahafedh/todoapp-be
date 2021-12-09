cb3a98400a0e0198f3ea13e91744877c06487cca
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2021-12-09 21:54:31 +0100

    not optimised

--- database_connection.js
+++ database_connection.js
@@ -7,9 +7,11 @@ const connection = mysql.createConnection({
 });
 
 const query = function query(query,operation){
-    connection.query(query, (err) => {
-        if (err) throw err;
+    connection.query(query, (err,res) => {
         console.log('#######{ '+operation+' }#######');
+        if (operation==='update' || operation==='select' ){console.log(res);};
+        if (err) throw err;
+        
         });
 };
 
--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-const db=require('./database_connection');
-const Joi =require('joi');
+const db = require("./database_connection");
+const Joi = require("joi");
 const express = require("express");
 const app = express();
-app.use(express.json())
-// db.query (`INSERT INTO tasks VALUES(1,'yh','h',NOW(),'t','h');`,'insertion');
+app.use(express.json());
 
+// db.query (`TRUNCATE TABLE tasks;`,'delete')
 
-// db.query (`TRUNCATE TABLE tasks;`,'data cleared')
+const port = process.env.PORT || 3000;
 
+function validate(input) {
+  const schema = {
+    name: Joi.string().required(),
+  };
+  return Joi.valid(input, schema);
+}
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}....`);
+});
+
+app.get("/", (req, res) => {
+  
+  res.send(db.query("SELECT * FROM tasks;",'selection'));
+});
+
+// app.get ('/add',(req,res)=>{
+//   res.send(db.query(`INSERT INTO TASKS VALUES (##################)`,'insertion'))
+// });
+
+app.get ('/high',(req,res)=>{
+  res.send (db.query(`SELECT * FROM TASKS 
+                    WHERE 'priority'='High';`,'selection'))
+});
+
+app.get ("/today",(req,res)=>{
+  res.send(db.query(`SELECT * FROM TASKS 
+                    WHERE 'due_on'=GETDATE(); `,'selection'))
+});
+
+app.get( '/home',(req,res)=>{
+  res.send(db.query(`SELECT * FROM TASKS 
+                    WHERE 'type'='home';`))
+});
+
+app.get( '/school',(req,res)=>{
+  res.send(db.query(`SELECT * FROM TASKS 
+                    WHERE 'type'='school';`))
+});
