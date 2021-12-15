bc9489c7ffd7f29de8bd2b2e65fe3e4803c13dd7
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2021-12-15 02:26:29 +0100

    ey

--- index.js
+++ index.js
@@ -20,43 +20,70 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  const getall = services.getall();
-  if (err) throw res.status(404).json(err);
-  res.status(200).json(getall);
+  try{
+    const getall = services.getall();
+    res.status(200).json(getall);
+  }catch(err){
+    console.error(err);
+  }
 });
 
 app.get("/high", (req, res) => {
-  const get = services.get("*", "priority", "High");
-  if (err) throw res.status(404).json(err);
-  res.status(200).json(get);
+  try{
+    const get = services.get("*", "priority", "High");
+  
+  res.status(200).json(get);}
+  catch(err){
+    console.error(err);
+  }
 });
 
 app.get("/today", (req, res) => {
-  const get = services.get("*", "due_on", "GETDATE()");
-  if (err) throw res.status(404).json(err);
-  res.status(200).json(get);
+  try{
+    const get = services.get("*", "due_on", "GETDATE()");
+  
+  res.status(200).json(get);}
+  catch(err){
+    console.error(err);
+  }
 });
 
 app.get("/home", (req, res) => {
-  const get = services.get("*", "type", "home");
-  if (err) throw res.status(404).json(err);
-  res.status(200).json(get);
+  try{
+    const get = services.get("*", "type", "home");
+  
+  res.status(200).json(get);}
+  catch(err){
+    console.error(err);
+  }
 });
 
 app.get("/school", (req, res) => {
-  const get = services.get("*", "type", "school");
-  if (err) throw res.status(404).json(err);
-  res.status(200).json(get);
+  try{
+    const get = services.get("*", "type", "school");
+  
+  res.status(200).json(get);}
+  catch(err){
+    console.error(err);
+  }
 });
 
 app.post("/create", (req, res) => {
-  const create = services.create("1", "test", "test", "test", "test", "test");
-  if (err) throw res.status(404).json(err);
-  res.status(200).json(create);
+  try{const create = services.create("1", "test", "test", "test", "test", "test");
+      res.status(200).json(create)}
+  catch(err){
+    console.error(err);
+  };
 });
 
 app.delete("/delete", (req, res) => {
-  const del = services.del("status", "done");
-  if (err) throw res.status(404).json(err);
-  res.status(200).json(del);
+  try{
+    const del = services.del("status", "done");
+  
+  res.status(200).json(del);}
+  catch(err){
+    console.error(err);
+  }
 });
+
+app.update
