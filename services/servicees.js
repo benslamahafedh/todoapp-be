03cca1fbdf98e4cef6041d9220fc88f8ef252f03
Hafedh Ben Slama <hafedhbenslama14@gmail.com>
2022-01-29 22:47:52 +0100

    fully functional stable not optimised services

--- services/servicees.js
+++ services/servicees.js
@@ -1,5 +1,6 @@
 const db = require("./../database_connection");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
@@ -8,57 +9,88 @@ app.listen(port, () => {
     console.log(`listening on port ${port}....`);
 });
 
-const  getall= ()=>{
+const  getAll= ()=>{
     try{
-        app.get('/',(req,res)=>{
-            const response = db.main("SELECT * FROM tasks;");
+        app.get('/',async (req,res)=>{
+            const response = await db.main("SELECT * FROM tasks;");            
             res.status(200).json(response)
+            
         })
     }catch(err){
+        res.status(400).send("bad request 400")
         console.error(err);
     };
 };
 
-// const get =(arg)=>{
-//     app.get(`/tasks/:id`,(req,res)=>{
-//         try{
-//             const arg = req.params.id;
-//             const response = db.main(`SELECT FROM tasks WHERE id=${arg};`);
-//             res.status(200).send(response);
-//         }catch(err){
-//             console.error(err);
-//         };
-//     });
-// };
+const get =()=>{
+    app.get(`/tasks/:id`,async(req,res)=>{
+        try{
+            const arg = req.params.id;
+            const rslt = await db.main(`SELECT * FROM tasks WHERE id = ${arg} ;`);
+            if (!rslt){
+                res.status(204).send("204 no content")
+            }else{
+                res.status(200).json(rslt);
+            }
+            
+        }catch(err){
+            res.status(400).send("bad request 400")
+            console.error(err);
+        };
+    });
+};
 
-// const update =(updatedColoumn,value)=>{
-//     try {
-//         app.put (`/tasks/:id`,(req,res)=>{
-//             const task = tasks.find(t => t.id === parseInt(req.params.id));
-//             if (!course) res.status(404).send('Task with given ID not found')
-//             const id = req.params.id;
-//             const update = db.main(`UPDATE tasks
-//                                     SET ${updatedColoumn}=${value}
-//                                     WHERE id=${id};`);
-//             task.name=req.body.name;
-//         })
-//     }catch(err){console.error(err);}
+const update =()=>{
+    try {
+        app.put (`/tasks/:id/:col/:val`,async (req,res)=>{
+            const col = req.params.col
+            const val = req.params.val
+            const id = req.params.id;
+            const update = await db.main(`UPDATE tasks SET ${col}='${val}'WHERE id=${id};`);
+            const rslt = await db.main(`SELECT * FROM tasks WHERE id= ${id} ;`)
+            res.status(200).json(rslt)
+
+        })
+    }catch(err){
+        res.status(400).send('Bad Request')
+        console.error(err);}
     
-// };
+};
 
 
-// const create = (value1,value2,value3,value4,value6)=>{
-//     db.main(`INSERT INTO tasks 
-//                 VALUES (${value1},${value2},${value3},${value4},${value5},${value6}); `)
-// };
-// const del=(column,condition)=>{
-//     db.main(`DELETE FROM tasks WHERE ${column}=${condition};`)
-// };
+const create = ()=>{
+    try {
+        app.post('/:value1/:value2/:value3/:value4/:value5/:value6',async (req,res)=>{
+            
+            await db.main(`INSERT INTO tasks 
+                        VALUES (${req.params.value1},'${req.params.value2}','${req.params.value3}',${req.params.value4},'${req.params.value5}','${req.params.value6}'); `)
+            const rslt = await db.main(`SELECT * FROM tasks;`)
+            res.status(200).json(rslt)
+        }
+        )
+    }catch (err){
+        res.status(400).send('400 Bad Request')
+        console.error(err)
+    }
+}
+const del=()=>{
+    app.delete('/:column/:condition',async (req,res)=>{
+        try{
+            await db.main(`DELETE FROM tasks WHERE ${req.params.column}='${req.params.condition}';`)
+            const rslt = await db.main(`SELECT * FROM tasks;`)
+            res.status(200).json(rslt)
+        }catch(err){
+            res.status(400).send('400 Bad Request')
+            console.error(err)
+        }
+    })
+
+};
 
 
-module.exports.getall = getall;
-// module.exports.get = get;
-// module.exports.update = update;
-// module.exports.del = del;
-// module.exports.create = create;
+module.exports.getAll = getAll;
+module.exports.get = get;
+module.exports.update = update;
+module.exports.del = del;
+module.exports.create = create;
 // module.exports.router=app;
